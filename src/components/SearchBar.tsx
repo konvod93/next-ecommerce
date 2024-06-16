625252efa089e865cdc1229f0511c9a86e0dbb4a
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-06-16 16:57:51 +0300

    added navbar icins and modals

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -17,7 +17,7 @@ const SearchBar = () => {
     }
 
     return (
-        <form className="flex ic justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1" onSubmit={handleSearch}>
+        <form className="flex items-center justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1" onSubmit={handleSearch}>
             <input type="text" name="name" placeholder="Search" className="flex-1 bg-transparent outline-none"/>
             <button className="cursor-pointer">
                 <Image src="/search.png" alt="" width={16} height={16}/>
