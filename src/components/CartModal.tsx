ae380df7e5ae3d30e2acb875c05371a0cf69e612
Konstantin <konvod93@gmail.com>
2025-07-06 12:32:28 +0300

    edition of CartModal:  sylized cart

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -4,32 +4,41 @@ import Image from "next/image"
 
 const CartModal = () => {
     const cartItems = true
-    return (        
-        <div className="absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 f1ex flex-col gap-6 z-20]">
+    return (
+        <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 f1ex flex-col gap-6 z-20]">
             {!cartItems ? (
                 <div className="">Cart is Empty</div>
             ) : (
-                <div className="">
-                    <Image src="https://images.pexels.com/photos/2199293/pexels-photo-2199293.jpeg" alt="" width={72} height={96} className="object-cover rounded-md"/>
-                    <div className="">
-                        {/* TOP */}
-                        <div className="">
-                            {/* TITLE */}
+                // LIST
+                <div className="flex flex-col gap-8">
+                    {/* ITEM */}
+                    <div className="flex gap-4">
+                        <Image src="https://images.pexels.com/photos/2199293/pexels-photo-2199293.jpeg" alt="" width={72} height={96} className="object-cover rounded-md" />
+                        <div className="flex flex-col justify-between w-full">
+                            {/* TOP */}
                             <div className="">
-                                <h3>Product Name</h3>
-                                <div className="">$49</div>
+                                {/* TITLE */}
+                                <div className="flex items-center justify-between gap-8">
+                                    <h3 className="font-semibold">Product Name</h3>
+                                    <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                                </div>
+                                {/* DESC */}
+                                <div className="text-sm text-gray-500">
+                                    avalable
+                                </div>
                             </div>
-                            {/* DESC */}
-                            <div className="">
-                                avalable
+                            {/* BOTTOM */}
+                            <div className="flex justify-between text-sm">
+                                <span className="text-gray-500">Qty.2</span>
+                                <span className="text-blue-500">Remove</span>
                             </div>
                         </div>
-                        {/* BOTTOM */}
                     </div>
                 </div>
             )}
-            
-            </div>
+
+        </div>
+
     )
 }
 
