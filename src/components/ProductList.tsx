fcdb836225fcabd94924d2601c02a7ab6d084617
Konstantin <konvod93@gmail.com>
2025-07-08 13:03:27 +0300

    completed structure of ProductList

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -21,8 +21,39 @@ const ProduvtList = () => {
                         className="absolute object-cover rounded-md"
                          />
                 </div>
-            </Link>
+                <div className="flex justify-between">
+                    <span className="font-medium">Produvt Name</span>
+                    <span className="font-semibold">$49</span>
+                </div>
+                <div className="tex-sm text-gray-500">My description</div>
+                <button className="rounded-2xl ring-1 w-max ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
+            </Link> 
+            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+                <div className="relative w-full h-80">
+                    <Image
+                        src="https://images.pexels.com/photos/2199293/pexels-photo-2199293.jpeg"
+                        alt=""
+                        fill
+                        sizes="25vw"
+                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+                         />
+                    <Image
+                        src="https://images.pexels.com/photos/93398/pexels-photo-93398.jpeg"
+                        alt=""
+                        fill
+                        sizes="25vw"
+                        className="absolute object-cover rounded-md"
+                         />
+                </div>
+                <div className="flex justify-between">
+                    <span className="font-medium">Produvt Name</span>
+                    <span className="font-semibold">$49</span>
+                </div>
+                <div className="tex-sm text-gray-500">My description</div>
+                <button className="rounded-2xl ring-1 w-max ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
+            </Link>             
         </div>
+        
     );
 };
 
