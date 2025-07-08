20787f194ba7335f6214df0c6d0c088b7858dc9e
Konstantin <konvod93@gmail.com>
2025-07-08 13:24:08 +0300

    Added Categu oryList to Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CategoryList from "@/components/CategoryList"
 import ProduvtList from "@/components/ProductList"
 import Slider from "@/components/Slider"
 
@@ -11,7 +12,7 @@ const HomePage = () => {
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Categories</h1>
-        <ProduvtList />
+        <CategoryList />
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
