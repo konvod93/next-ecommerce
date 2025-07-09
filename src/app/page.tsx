306f736864dce5c709069c3cb9e7fb8303e470c5
Konstantin <konvod93@gmail.com>
2025-07-09 11:01:49 +0300

    setting of horisontal scrolling in CtegoruList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
         <h1 className="text-2xl">Featured Products</h1>
         <ProduvtList />
       </div>
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <div className="mt-24">
         <h1 className="text-2xl">Categories</h1>
         <CategoryList />
       </div>
