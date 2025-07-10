99b650ad0fd8a2803cdc0bd6e63bf1ef5e0e5922
Konstantin <konvod93@gmail.com>
2025-07-10 17:55:32 +0300

    copleted Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -81,7 +81,16 @@ const Footer = () => {
             {/* BOTTOM */}
             <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
                 <div className="">© 2024 Lama Shop</div>
-                <div className=""></div>
+                <div className="flex flex-col gap-8 md:flex-row">
+                    <div className="">
+                        <span className="text-gray-500 mr-4">Language</span>
+                        <span className="font-medium">United States | English</span>
+                    </div>
+                    <div className="">
+                        <span className="text-gray-500 mr-4">Currency</span>
+                        <span className="font-medium">$ USD</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
