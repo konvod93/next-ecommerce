541b395e8225c219b598946f89247e3bcb752976
Konstantin <konvod93@gmail.com>
2025-07-19 18:19:29 +0300

    complete Filter component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,10 +1,77 @@
 
 const Filter = () => {
-  return (
-    <div className="mt-12 flex justify-between">
-      <div className=""></div>
-    </div>
-  );
+    return (
+        <div className="mt-12 flex justify-between">
+            <div className="flex gap-6 flex-wrap">
+                <select
+                    name="type"
+                    id=""
+                    className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+                >
+                    <option>Type</option>
+                    <option value="physical">Physical</option>
+                    <option value="digital">Digital</option>
+                </select>
+                <input
+                    type="text"
+                    name="min"
+                    placeholder="min price"
+                    className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+                />
+                <input
+                    type="text"
+                    name="max"
+                    placeholder="max price"
+                    className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+                />
+                <select
+                    name="size"
+                    id=""
+                    className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+                >
+                    <option>Size</option>
+                    <option value="size">Size</option>
+                </select>
+                <select
+                    name="color"
+                    id=""
+                    className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+                >
+                    <option>Color</option>
+                    <option value="color">Color</option>
+                </select>
+                <select
+                    name="type"
+                    id=""
+                    className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+                >
+                    <option>Category</option>
+                    <option value="category">Category</option>
+                </select>
+                <select
+                    name="type"
+                    id=""
+                    className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+                >
+                    <option>All Filters</option>
+                    <option value="all filteers">All Filters</option>                    
+                </select>
+            </div>
+            <div className="">
+                <select
+                name=""
+                id=""
+                className="py-2 px-4 rounded-2xl text-xs font-medium bg-white ring-1 ring-gray-400"
+                >
+                    <option>Sort By</option>
+                    <option value="">Price (low to high)</option>
+                    <option value="">Price (high to low)</option>
+                    <option value="">Newest</option>
+                    <option value="">Oldest</option>
+                </select>
+            </div>
+        </div>
+    );
 };
 
 export default Filter;
