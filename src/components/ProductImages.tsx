884a438d92dbf0cf1aa123e6f93d2bfebcf273d4
Konstantin <konvod93@gmail.com>
2025-07-24 12:54:14 +0300

    editing ProductImages

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -1,10 +1,44 @@
+import Image from "next/image";
 
 const ProductImages = () => {
-  return (
-    <div className="">
-      ProductImages
-    </div>
-  );
+    return (
+        <div className="">
+            <div className="h-[500px] relative">
+                <Image
+                    src="https://images.pexels.com/photos/28997255/pexels-photo-28997255.jpeg"
+                    alt=""
+                    fill
+                    sizes="50vw"
+                    className="object-cover rounded-md"
+                />
+            </div>
+            <div className="">
+                <div className="w-1/4 h-32 relative gap-4 mt-8">
+                    <Image
+                        src="https://images.pexels.com/photos/28997255/pexels-photo-28997255.jpeg"
+                        alt=""
+                        fill
+                        sizes="30vw"
+                        className="object-cover rounded-md"
+                    />
+                    <Image
+                        src="https://images.pexels.com/photos/28997255/pexels-photo-28997255.jpeg"
+                        alt=""
+                        fill
+                        sizes="30vw"
+                        className="object-cover rounded-md"
+                    />
+                    <Image
+                        src="https://images.pexels.com/photos/28997255/pexels-photo-28997255.jpeg"
+                        alt=""
+                        fill
+                        sizes="30vw"
+                        className="object-cover rounded-md"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ProductImages;
