48b57bbe99be87e489dc63dc02c1095bd79085bd
Konstantin <konvod93@gmail.com>
2025-07-31 13:13:03 +0300

    tditing ProductImages

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,7 +2,7 @@ import ProductImages from "@/components/ProductImages"
 
 const SinglePage = () => {
   return (
-    <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
+    <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
       {/* IMG */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
         <ProductImages />
--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -1,41 +1,58 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from "react";
+
+const images = [
+    {
+        id: 1,
+        url: 'https://images.pexels.com/photos/32508907/pexels-photo-32508907.jpeg',
+
+    },
+    {
+        id: 2,
+        url: 'https://images.pexels.com/photos/33175235/pexels-photo-33175235.jpeg',
+        
+    },
+    {
+        id: 3,
+        url: 'https://images.pexels.com/photos/31685539/pexels-photo-31685539.jpeg',
+        
+    },
+    {
+        id: 4,
+        url: 'https://images.pexels.com/photos/33210292/pexels-photo-33210292.jpeg',
+        
+    }
+];
 
 const ProductImages = () => {
+
+    const [index, setIndex] = useState(0);
+
     return (
         <div className="">
             <div className="h-[500px] relative">
                 <Image
-                    src="https://images.pexels.com/photos/28997255/pexels-photo-28997255.jpeg"
+                    src={images[index].url}
                     alt=""
                     fill
                     sizes="50vw"
                     className="object-cover rounded-md"
                 />
             </div>
-            <div className="">
-                <div className="w-1/4 h-32 relative gap-4 mt-8">
+            <div className="flex justify-between gap-4 mt-8">
+                {images.map((img, i) =>  (
+                    <div className="w-1/4 h-32 relative gap-4 mt-8" key={img.id} onClick={() => setIndex(i)}>
                     <Image
-                        src="https://images.pexels.com/photos/28997255/pexels-photo-28997255.jpeg"
+                        src={img.url}
                         alt=""
                         fill
                         sizes="30vw"
                         className="object-cover rounded-md"
-                    />
-                    <Image
-                        src="https://images.pexels.com/photos/28997255/pexels-photo-28997255.jpeg"
-                        alt=""
-                        fill
-                        sizes="30vw"
-                        className="object-cover rounded-md"
-                    />
-                    <Image
-                        src="https://images.pexels.com/photos/28997255/pexels-photo-28997255.jpeg"
-                        alt=""
-                        fill
-                        sizes="30vw"
-                        className="object-cover rounded-md"
-                    />
+                    />                    
                 </div>
+                ))}
             </div>
         </div>
     );
