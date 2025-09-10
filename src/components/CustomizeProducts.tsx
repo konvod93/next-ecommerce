c3ada6d0fe8e7b64df6bd7b58809148a8774d094
Konstantin <konvod93@gmail.com>
2025-09-10 15:02:13 +0300

    editing CustomizeProducts

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -1,6 +1,13 @@
 const CustomizeProducts = () => {
     return (
-        <div className="p-4 border rounded-md">CustomizeProducts</div>
+        <div className="flex flex-col gap-6">
+            <h4 className="font-medium">Choose a color</h4>
+            <ul className="flex items-center gap-3">
+                <li className="w-8 h-8 rounded-full ring-1 ring-gray"></li>
+            </ul>
+            <h4 className="font-medium">Choose a size</h4>
+            <ul className="flex items-center gap-3"></ul>
+        </div>
     )
 }
 export default CustomizeProducts
