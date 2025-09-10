ce7590fa2592ee742863642d5c639e49c9db1fb4
Konstantin <konvod93@gmail.com>
2025-09-10 18:20:51 +0300

    edited choose a size section in CustomProducts

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -13,7 +13,9 @@ const CustomizeProducts = () => {
             </ul>
             <h4 className="font-medium">Choose a size</h4>
             <ul className="flex items-center gap-3">
-                
+                <li className="ring-1 ring-lama text-lama rounded-md py-1 px-4 text-sm cursor-pointer">Small</li>
+                <li className="ring-1 ring-lama text-white bg-lama rounded-md py-1 px-4 text-sm cursor-not-allowed">Medium</li>
+                <li className="ring-1 ring-pink-200 text-white bg-pink-200 rounded-md py-1 px-4 text-sm cursor-not-allowed">Large</li>
             </ul>
         </div>
     )
