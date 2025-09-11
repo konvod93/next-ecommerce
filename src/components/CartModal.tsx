26bb48d3b0564e29c1ece45b9434d8124a878f00
Konstantin <konvod93@gmail.com>
2025-09-11 17:41:29 +0300

    fixing bugs of design

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 const CartModal = () => {
     const cartItems = true
     return (
-        <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 f1ex flex-col gap-6 z-20]">
+        <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 f1ex flex-col gap-6 z-20] z-20">
             {!cartItems ? (
                 <div className="">Cart is Empty</div>
             ) : (                
