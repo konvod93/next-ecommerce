99bf2cd504bc0d4bea6edb484e2abdfaeede6f1c
Konstantin <konvod93@gmail.com>
2025-09-11 17:05:14 +0300

    completed Add SInglePage design

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -6,11 +6,18 @@ const Add = () => {
   return (
     <div className="flex flex-col gap-4">
       <h4 className="font-medium">Choose a Quantity</h4>
-      <div className="">
-        <div className=""></div>
-        <div className=""></div>
+      <div className="flex justify-between">
+        <div className="flex items-center gap-4">
+          <div className="bg-gray-100 py-2 px-4 rounded-3xl flex items-center justify-between w-32">
+            <button className="cursor-pointer text-xl">-</button>
+            {quantity}
+            <button className="cursor-pointer text-xl">+</button>
+          </div>
+          <div className="text-xs">Only <span className="text-orange-500">4 items</span> left!<br /> {"Dont't"} miss it</div>
+        </div>
+        <button className="w-36 text-sm rounded-3xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none">Add to Cart</button>
       </div>
-      <button className="w-36 text-sm rounded-3xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none">Add to Cart</button>
+
     </div>
   );
 };
