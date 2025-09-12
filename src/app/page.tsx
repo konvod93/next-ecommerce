e1eafac486a74fc0fc6f53ee3324baaaaf14347c
Konstantin <konvod93@gmail.com>
2025-09-12 16:25:05 +0300

    fixwd bug in HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ const HomePage = async () => {
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
         <Suspense fallback={"Loading..."}>
-          <ProductList categoryId={process.env.NEXT_PUBLIC_FEATURED_PRODUCTS_CATEGORY_ID} limit={4} />
+          <ProductList categoryId={process.env.NEXT_PUBLIC_FEATURED_PRODUCTS_CATEGORY_ID ?? ""} limit={4} />
         </Suspense>        
       </div>
       <div className="mt-24">
@@ -36,7 +36,7 @@ const HomePage = async () => {
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        <ProduvtList />
+        <ProductList categoryId={process.env.NEXT_PUBLIC_FEATURED_PRODUCTS_CATEGORY_ID ?? ""} limit={4} />
       </div>
     </div>
   )
