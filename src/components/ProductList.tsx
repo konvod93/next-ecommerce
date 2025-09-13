fa9fae9437dbd1c2d096ab1a5650109556c850c4
Konstantin <konvod93@gmail.com>
2025-09-13 19:44:14 +0300

    added short description fetching to ProductList

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { wixClientServer } from "../lib/wixClientServer";
 import { products } from "@wix/stores";
+import DOMPurify from "isomorphic-dompurify";
 
 const PRODUCT_PER_PAGE = 20;
 
@@ -9,14 +10,14 @@ const ProductList = async ({ categoryId, limit }: { categoryId: string; limit?: 
 
     const wixClient = await wixClientServer();
     const res = await wixClient.products.queryProducts().eq("collectionIds", categoryId).limit(limit || PRODUCT_PER_PAGE).find();
-    
+
     return (
         <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
             {res.items.map((product: products.Product) => (
-                <Link 
-                href={"/"+product.slug}
-                key={product._id} 
-                className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+                <Link
+                    href={"/" + product.slug}
+                    key={product._id}
+                    className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
                 >
                     <div className="relative w-full h-80">
                         <Image
@@ -27,13 +28,13 @@ const ProductList = async ({ categoryId, limit }: { categoryId: string; limit?: 
                             className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
                         />
                         {product.media?.items && (
-                        <Image
-                            src={product.media?.items[1]?.image?.url || "/product.png"}
-                            alt=""
-                            fill
-                            sizes="25vw"
-                            className="absolute object-cover rounded-md"
-                        />
+                            <Image
+                                src={product.media?.items[1]?.image?.url || "/product.png"}
+                                alt=""
+                                fill
+                                sizes="25vw"
+                                className="absolute object-cover rounded-md"
+                            />
                         )}
                     </div>
                     <div className="flex justify-between">
@@ -45,7 +46,15 @@ const ProductList = async ({ categoryId, limit }: { categoryId: string; limit?: 
                             }) ?? "N/A"}
                         </span>
                     </div>
-                    <div className="tex-sm text-gray-500">My description</div>
+                    {product.additionalInfoSections && (
+                        <div
+                            className="tex-sm text-gray-500"
+                            dangerouslySetInnerHTML={{
+                                __html: DOMPurify.sanitize(product.additionalInfoSections.
+                                    find((section: any) => section.title === "shortDesc")?.description || "")
+                            }}
+                        ></div>
+                    )}
                     <button className="rounded-2xl ring-1 w-max ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
                 </Link>
             ))}
