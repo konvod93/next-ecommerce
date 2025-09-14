d12f48f856675742e61033e6a4ffaa9be6b11009
Konstantin <konvod93@gmail.com>
2025-09-14 15:03:46 +0300

    fetch categories

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,8 +1,15 @@
 import Filter from "@/components/Filter"
-import ProduvtList from "@/components/ProductList"
+import ProductList from "@/components/ProductList"
 import Image from "next/image"
+import { wixClientServer } from "@/lib/wixClientServer"
+import { collections } from "@wix/stores"
+import { Suspense } from "react"
 
-const ListPage = () => {
+
+const ListPage = async ({ searchParams }: { searchParams: any }) => {
+    const wixClient = await wixClientServer();
+    const cat = await wixClient.collections.getCollectionBySlug(searchParams.cat || "");
+    
     return (
         <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
             {/* CAMPAIGN */}
@@ -19,7 +26,9 @@ const ListPage = () => {
             <Filter />
             {/* PRODUCTS */}
             <h1 className="mt-12 text-xl font-semibold">Shoes For You!</h1>
-            <ProduvtList />
+            <Suspense fallback={"Loading..."}>
+                <ProductList categoryId={cat.collection?._id || "00000000-000000-000000-000000000001"} searchParams={searchParams} />
+            </Suspense>
         </div>
     )
 }
--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,49 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
+import { wixClientServer } from "../lib/wixClientServer";
 
-const ComponentName = () => {
+const CategoryList = async () => {
+    const wixClient = await wixClientServer();
+    const cats = await wixClient.collections.queryCollections().find()
     return (
         <div className="px-4 overflow-x-scroll scrollbar-hide">
             <div className="flex gap-4 md:gap-8">
-                <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
-                    <div className="relative bg-slate-100 w-full h-96">
-                        <Image src="https://images.pexels.com/photos/1044290/pexels-photo-1044290.jpeg" alt="" fill sizes="20wh" className="object-cover" />
-                    </div>
-                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
-                </Link>
-                <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
-                    <div className="relative bg-slate-100 w-full h-96">
-                        <Image src="https://images.pexels.com/photos/1044290/pexels-photo-1044290.jpeg" alt="" fill sizes="20wh" className="object-cover" />
-                    </div>
-                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
-                </Link>
-                <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
-                    <div className="relative bg-slate-100 w-full h-96">
-                        <Image src="https://images.pexels.com/photos/1044290/pexels-photo-1044290.jpeg" alt="" fill sizes="20wh" className="object-cover" />
-                    </div>
-                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
-                </Link>
-                <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
-                    <div className="relative bg-slate-100 w-full h-96">
-                        <Image src="https://images.pexels.com/photos/1044290/pexels-photo-1044290.jpeg" alt="" fill sizes="20wh" className="object-cover" />
-                    </div>
-                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
-                </Link>
-                <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
-                    <div className="relative bg-slate-100 w-full h-96">
-                        <Image src="https://images.pexels.com/photos/1044290/pexels-photo-1044290.jpeg" alt="" fill sizes="20wh" className="object-cover" />
-                    </div>
-                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
-                </Link>
-                <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
-                    <div className="relative bg-slate-100 w-full h-96">
-                        <Image src="https://images.pexels.com/photos/1044290/pexels-photo-1044290.jpeg" alt="" fill sizes="20wh" className="object-cover" />
-                    </div>
-                    <h1 className="mt-8 font-light text-cl tracking-wide">Category Name</h1>
-                </Link>
+                {cats.items.map(item => (
+                    <Link
+                        href={`/list?cat=${item.slug}`}
+                        className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+                        key={item._id}>
+                        <div className="relative bg-slate-100 w-full h-96">
+                            <Image
+                                src={item.media?.mainMedia?.image?.url || "/category.png"}
+                                alt=""
+                                fill
+                                sizes="20wh"
+                                className="object-cover"
+                            />
+                        </div>
+                        <h1 className="mt-8 font-light text-cl tracking-wide">{item.name}</h1>
+                    </Link>
+                ))}
             </div>
         </div>
     );
 };
 
-export default ComponentName;
+export default CategoryList;
